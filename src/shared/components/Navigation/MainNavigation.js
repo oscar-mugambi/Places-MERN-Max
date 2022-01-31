b4c7d92835210514461c8903c7508b4339c5e45e
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-31 18:11:03 +0300

    added nav link

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
+import NavLinks from './NavLinks';
 
 const MainNavigation = () => {
   return (
     <MainHeader>
       <button className='main-navigation__menu-btn'>
-        <span></span>
-        <span></span>
-        <span></span>
+        <span />
+        <span />
+        <span />
       </button>
 
       <h1 className='main-navigation__title'>
         <Link to='/'>Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -3,7 +3,22 @@ import { NavLink } from 'react-router-dom';
 import './NavLinks.css';
 
 const NavLinks = (props) => {
-  return <ul className='nav-links'></ul>;
+  return (
+    <ul className='nav-links'>
+      <li>
+        <NavLink to='/'>ALL USERS</NavLink>
+      </li>
+      <li>
+        <NavLink to='/1/places'>MY PLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to='/places/new'>ADD PLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to='/auth'>AUTHENTICATE</NavLink>
+      </li>
+    </ul>
+  );
 };
 
 export default NavLinks;
