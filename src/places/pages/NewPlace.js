ebefeceafa9d0fb5774b712f34dc59a5152eea41
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-31 22:11:10 +0300

    added reusable Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
 
 const NewPlace = () => {
-  return <div>Hey there</div>;
+  return (
+    <form className='place-form'>
+      <Input element='input' type='text' label='Title' />
+    </form>
+  );
 };
 
 export default NewPlace;
