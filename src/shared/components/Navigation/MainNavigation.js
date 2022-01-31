b8438b5ce9063591e5ba6901ebdfd93a7aeda1ff
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-31 18:36:57 +0300

    added backdrop portal

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
-import './MainNavigation.css';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = () => {
-  return (
-    <MainHeader>
-      <button className='main-navigation__menu-btn'>
-        <span />
-        <span />
-        <span />
-      </button>
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-      <h1 className='main-navigation__title'>
-        <Link to='/'>Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className='main-navigation__drawer-nav'>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+
+        <h1 className='main-navigation__title'>
+          <Link to='/'>Your Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
