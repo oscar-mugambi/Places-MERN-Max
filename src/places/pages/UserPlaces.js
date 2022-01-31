d72ae8395d962252e85a53b3f31cc6a97bb630e6
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-31 19:54:45 +0300

    added shares button

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList';
 
-const UserPlaces = () => {
-  const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Nairobi',
-      description: 'famous city',
-      imageUrl:
-        'https://images.pexels.com/photos/7519948/pexels-photo-7519948.jpeg?cs=srgb&dl=pexels-jamiesx-co-7519948.jpg&fm=jpg',
-      address: '20 W 34th St, Nairobi, KE 10001',
-      location: {
-        lat: -1.2538892,
-        lon: 36.5147118,
-      },
-      creatorId: 'u1',
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Nairobi',
+    description: 'famous city',
+    imageUrl:
+      'https://images.pexels.com/photos/7519948/pexels-photo-7519948.jpeg?cs=srgb&dl=pexels-jamiesx-co-7519948.jpg&fm=jpg',
+    address: '20 W 34th St, Nairobi, KE 10001',
+    location: {
+      lat: -1.2538892,
+      lon: 36.5147118,
     },
-  ];
+    creatorId: 'u1',
+  },
+  {
+    id: 'p1',
+    title: 'Nairobi',
+    description: 'famous city',
+    imageUrl:
+      'https://images.pexels.com/photos/7519948/pexels-photo-7519948.jpeg?cs=srgb&dl=pexels-jamiesx-co-7519948.jpg&fm=jpg',
+    address: '20 W 34th St, Nairobi, KE 10001',
+    location: {
+      lat: -1.2538892,
+      lon: 36.5147118,
+    },
+    creatorId: 'u2',
+  },
+];
+
+const UserPlaces = () => {
+  const { userId } = useParams();
+
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creatorId === userId);
 
   return (
     <div>
-      <PlaceList items={DUMMY_PLACES} />
+      <PlaceList items={loadedPlaces} />
     </div>
   );
 };
