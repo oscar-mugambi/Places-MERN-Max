cd33d11c2fbb4674bab56155e71a68d801d9f052
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-31 18:51:06 +0300

    added transitions

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -20,7 +20,7 @@ const MainNavigation = () => {
     <>
       {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
       {drawerIsOpen && (
-        <SideDrawer>
+        <SideDrawer show={drawerIsOpen}>
           <nav className='main-navigation__drawer-nav'>
             <NavLinks />
           </nav>
