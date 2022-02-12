bf1dd7894a2d8b725caeddfc2a9ce2e53974bb05
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-12 23:29:02 +0300

    minor fix

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -40,7 +40,6 @@ const Input = (props) => {
   }, [id, value, isValid, onInput]);
 
   const changeHandler = (e) => {
-    // @ts-ignore
     dispatch({
       type: 'CHANGE',
       val: e.target.value,
