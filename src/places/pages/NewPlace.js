c85435fde2631a02b8f1ac287521c60a1b6cea43
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-13 20:48:43 +0300

    added reducer for form inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
 
 import './NewPlace.css';
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+
+      return {};
+
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  const [state, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
   const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
 
   return (
     <form className='place-form'>
@@ -24,7 +58,7 @@ const NewPlace = () => {
         label='Description'
         validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
         errorText='Please enter a valid description'
-        onInput={titleInputHandler}
+        onInput={descriptionInputHandler}
       />
     </form>
   );
