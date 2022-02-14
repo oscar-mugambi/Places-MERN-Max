3b0f0699e282a452ee516587efc2e409aa2e3594
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-14 23:06:14 +0300

    initialized context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
+import { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <>
+        <Route path='/' element={<Users />} />
+        <Route path='/:userId/places' element={<UserPlaces />} />
+        <Route path='/places/new' element={<NewPlace />} />
+        <Route path='/places/:placeId' element={<UpdatePlace />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </>
+    );
+  } else {
+    routes = (
+      <>
+        <Route path='/' element={<Users />} />
+        <Route path='/:userId/places' element={<UserPlaces />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='*' element={<Navigate to='/auth' />} />
+      </>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path='/' element={<Users />} />
-          <Route path='/places/new' element={<NewPlace />} />
-          <Route path='/:userId/places' element={<UserPlaces />} />
-          <Route path='/places/:placeId' element={<UpdatePlace />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>{routes}</Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
@@ -9,9 +9,12 @@ import {
   VALIDATOR_REQUIRE,
 } from '../../shared/components/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
+
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -54,6 +57,7 @@ const Auth = () => {
 
   const authSubmitHandler = (event) => {
     event.preventDefault();
+    auth.login();
     console.log(formState.inputs);
   };
 
