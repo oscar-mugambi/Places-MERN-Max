d72836dae3ec33dfa536ea6057334bac2a53e441
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-14 22:29:31 +0300

    initialized context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/places/new' element={<NewPlace />} />
           <Route path='/:userId/places' element={<UserPlaces />} />
           <Route path='/places/:placeId' element={<UpdatePlace />} />
+          <Route path='/auth' element={<Auth />} />
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </main>
